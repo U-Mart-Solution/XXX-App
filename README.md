e725dbc08a39507dfa359315f1f19d9b646a9581
ushan121 <ushanshanilka80@gmail.com>
2021-10-01 16:36:02 +0530

    initialized project

---
+++ README.md
@@ -0,0 +1,10 @@
+ This Project is XXX Project
+ 
+app run
+    setup gradle and run   
+
+app build
+    grable build
+
+deployment instruction  
+    build and get jar then deploy 
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) U-Mart. All rights reserved.
+ *  *  Licensed under the SriLanka Information License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author Ushan Shanilka <ushanshanilka80@gmail.com>
+ * @since 10/1/2021
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello XXX");
+    }
+}
